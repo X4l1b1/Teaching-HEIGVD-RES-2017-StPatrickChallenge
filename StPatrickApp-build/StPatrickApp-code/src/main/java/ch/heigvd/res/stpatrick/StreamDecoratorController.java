40b67849f454f10eaac8e3c2ff935471c34988f7
Arthur <arthur.passuello@heig-vd.ch>
2017-03-20 12:37:59 +0100

    Tried to implement the DecoratedReader and Writer..

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamDecoratorController.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamDecoratorController.java
@@ -11,12 +11,26 @@ class StreamDecoratorController implements IStreamDecoratorController {
 
 
     Reader decorateReader(Reader inputReader){
+        return new DecoratedReader extends BufferedReader (inputReader) {
+                public int read(){
+                        int c = super.read();
+                        while((char) c == 'a' || (char) c == 'A'){
+                             c = super.read();
+                        }
+                        return c;
 
+                }
+        }
     }
 
 
     Writer decorateWriter(Writer outputWriter){
+        return new DecoratedWriter extends BufferedWriter (outputWriter) {
 
-
+            public void write(int c){
+                if((char) c == 'a' || (char) c == 'A') { return;}
+                super.write(c);
+            }
+        }
     }
 }
